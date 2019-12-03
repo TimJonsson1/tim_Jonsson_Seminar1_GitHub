a69c4137afc5f7294c0700245d1f1c5c2645a734
TimJonsson1 <44998865+TimJonsson1@users.noreply.github.com>
2019-12-03 10:30:05 +0100

    added Sub method in the main class

--- src/hkr/Main.java
+++ src/hkr/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
     }
+
+    public int add (int first, int second){
+        return (first + second);
+    }
+
 }
